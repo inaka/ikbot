f27b300505140e8209b56daf363e5b0444421768
Marcos Almonacid <marcos@inaka.net>
2014-12-18 18:49:08 -0300

    fix devops

--- lib/ikbot/scripts/devops.ex
+++ lib/ikbot/scripts/devops.ex
@@ -3,31 +3,37 @@ defmodule Ikbot.Script.Devops do
 
   def base _message do
     case HTTPotion.get("http://devopsreactions.tumblr.com/random", [], []) do
-      %Response{headers: headers, status_code: 302} ->
+      %Response{headers: headers, status_code: 302, body: body} ->
         location = headers[:Location]
-        title = get_title location
-        url = get_url location
-        "#{title}#{url}"
+        {title, url} = get_info(location)
+        "#{title} #{url}"
       %Response{status_code: status_code} ->
         "Sorry. Something went wrong (code: #{status_code})"
     end
   end
 
-  defp get_title(location) do
-    {:ok, regex} = Regex.compile("([a-z 0-9]|-)*#")
-    [slug | _] = Regex.run(regex, location)
-    String.replace(slug, ~r/-|#/, " ")
-  end
-
-  defp get_url(location) do
+  defp get_info(location) do
     [location_url | _] = String.split(location, "#")
     case HTTPotion.get(location_url) do
-      %Response{body: body, status_code: 200} ->
-        {:ok, regex} = Regex.compile("src=.*.gif")
-        [img_tag | _] = Regex.run(regex, body)
-        String.replace(img_tag, "src=\"", "")
+      %Response{headers: headers, body: body, status_code: 200} ->
+        title = get_title(body)
+        url = get_url(body)
+        {title, url}
       %Response{status_code: status_code} ->
         {"Sorry. Something went wrong (code: #{status_code})", ""}
     end
+  end
+
+  defp get_title(body) do
+    {:ok, reg1} = Regex.compile("class=\\\"post_title\\\">.*</a>")
+    {:ok, reg2} = Regex.compile("class=\\\"post_title\\\"><a .*\">|</a>")
+    [tag | _] = Regex.run(reg1, body)
+    String.replace(tag, reg2, "")
+  end
+
+  defp get_url(body) do
+    {:ok, regex} = Regex.compile("src=.*.gif")
+    [img_tag | _] = Regex.run(regex, body)
+    String.replace(img_tag, "src=\"", "")
   end
 end
